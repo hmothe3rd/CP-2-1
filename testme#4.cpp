f3bad261fc1639dc41d1cc094d03d4c76fdc64d5
hmothe3rd <152071594+hmothe3rd@users.noreply.github.com>
2024-01-04 11:48:40 +0800

    Update testme#4.cpp

--- testme#4.cpp
+++ testme#4.cpp
@@ -1,39 +1,20 @@
 #include <iostream>
-#include <limits>
 
 using namespace std;
 
-// Function declaration
-float Add2Values(float, float);
+float Add2Values(float x, float y);
 
 int main()
 {
-    char x, y;
+    float x, y;
 
-    cout 
-        << "-- Add 2 Values --\n"
-        << "Enter x Value: ";
-    
+    cout << "-- Add 2 Values --\n"
+         << "Enter x Value: ";
+
     cin >> x;
-   
-    while (cin.fail()) {
-        
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid input. Enter a numeric value for x: ";
-        cin >> x;
-    }
 
     cout << "Enter y Value: ";
     cin >> y;
-    
-    while (cin.fail()) {
-        
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid input. Enter a numeric value for y: ";
-        cin >> y;
-    }
 
     cout << "__________________\n";
     
